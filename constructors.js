176f8c0fc2d8ae668ec294a8c47a9ac5c1451011
Rana Mohammad <ranamohammad@Ranas-MacBook-Pro.local>
2018-06-30 16:15:28 -0500

    done

--- constructors.js
+++ constructors.js
@@ -14,6 +14,12 @@
 */
 
 // Code here
+function CarFactory(make, model){
+
+  this.make = make,
+  this.model = model
+
+}
 
 
 
@@ -33,6 +39,7 @@ function Employee(name, email, hireDate) {
 */
 
 // Code here
+const bob = new Employee('Bob', 'bob@gmail.com', '01-02-98')
 
 
 
@@ -54,5 +61,14 @@ mustang.moveCar(); // Increments mustang' move property by 10. Returns the new m
 */
 
 // Code here
-
+function Car(make, model, year){
+  this.make = make
+  this.model = model
+  this.year = year
+  this.move = 0
+  this.moveCar = function(){
+    this.move += 10
+    return this.move
+    }
+  }
 
